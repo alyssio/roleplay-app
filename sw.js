1e9baafe0a78b7ece8a061a43e942aede41c488c
alyssio <tomlutiroger@gmail.com>
2026-03-14 15:11:08 -0400

    service worker forces reload on update for safari webapp

--- sw.js
+++ sw.js
@@ -1,9 +1,15 @@
-const CACHE = 'roleplay-v1';
+const CACHE = 'roleplay-v2';
 
 self.addEventListener('install', () => self.skipWaiting());
 
 self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+      .then(() => self.clients.matchAll({ includeUncontrolled: true }))
+      .then(clients => clients.forEach(c => c.postMessage({ type: 'SW_UPDATED' })))
+  );
 });
 
 self.addEventListener('fetch', e => {
